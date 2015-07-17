75f3becec1660e08d7c7b80a1be6b458fa5b9db9
James Drew <j.drew1303@gmail.com>
2015-07-17 18:55:26 +0100

    now runs tests in parallel (splits tests across multiple browser) and runs the tests simultaneously on different browser types.

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -4,25 +4,25 @@ exports.config = {
 
   allScriptsTimeout: 11000,
 
-  baseUrl: 'http://localhost:3000/',
+  baseUrl: 'http://localhost:3002/',
 
-  capabilities: {
-    browserName: 'chrome',
-    version: '',
-    platform: 'ANY'
-  },
+  multiCapabilities: [
+    { browserName: 'firefox', shardTestFiles: true, maxInstances: 2 },
+    { browserName: 'chrome',  shardTestFiles: true, maxInstances: 2 }
+  ],
 
   framework: 'jasmine',
 
   jasmineNodeOpts: {
-    isVerbose: false,
+    isVerbose: true,
     showColors: true,
     includeStackTrace: true,
     defaultTimeoutInterval: 30000
   },
 
   specs: [
-    'e2e/**/*.js'
+    'e2e/**/*.js',
+    'test/factorySpec.js'
   ]
 
 };
