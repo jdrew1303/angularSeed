98ec68c36c335dd233288a2026e1431138798ec1
James Drew <j.drew1303@gmail.com>
2015-07-19 15:53:27 +0100

    cleanup and added comments

--- gulp/tasks/swaggerFactories.js
+++ gulp/tasks/swaggerFactories.js
@@ -1,9 +1,15 @@
-var gulp          = require('gulp');
-var _             = require('lodash-node');
-var config        = require('../config');
-var parser = require("swagger-parser");
-var fs = require('fs');
+var gulp    = require('gulp');
+var fs      = require('fs');
+var _       = require('lodash-node');
+var parser  = require("swagger-parser");
+var config  = require('../config');
 
+// Need to be careful, any circular references or malformed files will
+// throw an error. Also files that do not have definitions for objects
+// will fail to generate. You should pay careful attention to the
+// factories, especially child factories. Sometimes the naming scheme
+// for the fields and the objects are not the same. In future I hope
+// to make this more reliable.
 
 var allFactoryLibs = [];
 
@@ -19,10 +25,8 @@ function readModuleFile(path, err) {
   }
 }
 
-
 // Use multiprogramming to generate separate gulp functions for each of the apis
 // we've specified in the config file.
-
 _.map(config.swagger.src, function(source){
 
   var fileName = _.camelCase(source.moduleName) + 'Factories.js';
@@ -30,6 +34,24 @@ _.map(config.swagger.src, function(source){
 
   allFactoryLibs.push(generateFactoryForTaskName);
 
+  function flattenSwaggerModelTree(swagger){
+    var modelCache = [];
+
+    _.forOwn(swagger.definitions, function parseModels(modelObject, modelName){
+      // if were at the bottom layer of the tree break out of the recursion.
+      if(!modelObject.properties){ return; }
+
+      // We build the individual model to be turned into a factory.
+      var processedModel = {};
+      processedModel[modelName] = modelObject;
+      modelCache.push(processedModel);
+
+      // recurse down the tree and keep parsing.
+      _.forOwn(modelObject, parseModels);
+    });
+
+    return { swagger : modelCache};
+  }
 
   // This task parses the swagger files and generates test data factories.
   gulp.task(generateFactoryForTaskName, function(done) {
@@ -60,26 +82,6 @@ _.map(config.swagger.src, function(source){
 // This is the generic task to generate all client libraries specified in the
 // config files.
 gulp.task('factories', allFactoryLibs);
-
-
-function flattenSwaggerModelTree(swagger){
-  var modelCache = [];
-
-  _.forOwn(swagger.definitions, function parseModels(modelObject, modelName){
-    // if were at the bottom layer of the tree break out of the recursion.
-    if(!modelObject.properties){ return; }
-
-    // We build the individual model to be turned into a factory.
-    var processedModel = {};
-    processedModel[modelName] = modelObject;
-    modelCache.push(processedModel);
-
-    // recurse down the tree and keep parsing.
-    _.forOwn(modelObject, parseModels);
-  });
-
-  return { swagger : modelCache};
-}
 
 
 
