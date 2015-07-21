3decdaf26db4ec05d55d2f3164d1a8d31fbd9811
James Drew <j.drew1303@gmail.com>
2015-07-21 03:23:01 +0100

    config file should be tied to the folder the feature/page is attached to. This allows it to be tested and possibly run independently of the rest of the page.

--- app/js/onConfig.js
+++ app/js/onConfig.js
@@ -13,24 +13,6 @@ function OnConfig($stateProvider, $locationProvider, $urlRouterProvider) {
     controller: 'ExampleController as home',
     templateUrl: 'home.html',
     title: 'Home'
-  })
-  .state('Form', {
-    url: '/form',
-    controller: 'SubmitFormController as submit',
-    controllerAs: 'submit',
-    templateUrl: 'submitForm.html',
-    title: 'Form',
-    resolve: {
-      schema : ['$q', 'personSchema', function($q, personSchema){ return $q.when(personSchema); }],
-      form : ['$q', 'personForm', function($q, personForm){ return $q.when(personForm); }],
-      // load the model from a service to prepopulate data in form.
-      model : ['$q', function($q){ return $q.when({}); }],
-      // the service to submit the form to. Try to keep this api standard
-      // across all forms. This will allow you to reuse as much of the
-      // templates/controllers as possible. By doing this creating a new
-      // form is only about configuration adn wiring.
-      submitService : ['$q', function($q){ return $q.when({}); }]
-    }
   });
 
   $urlRouterProvider.otherwise('/');
