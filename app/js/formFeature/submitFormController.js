b370f2940df5c5a157dd05b63ea7b83f2d86b02b
James Drew <j.drew1303@gmail.com>
2015-07-23 22:14:05 +0100

    cleanup

--- app/js/formFeature/submitFormController.js
+++ app/js/formFeature/submitFormController.js
@@ -6,16 +6,16 @@ var featureModule = require('./_index');
  * @ngInject
  */
 function SubmitFormController($scope, schema, form, model, submitService) {
-  var vm = this;
-  vm.schemaJson = schema;
-  vm.formJson   = JSON.parse(JSON.stringify(form));
-  vm.modelData  = model || {};
+  console.log(schema);
+  this.schemaJson = schema;
+  this.formJson   = JSON.parse(JSON.stringify(form));
+  this.modelData  = model || {};
   //console.log($scope);
 
-  vm.submitForm = function(form, model) {
+  this.submitForm = function(form, model) {
     //console.log(model);
     // First we broadcast an event so all fields validate themselves
-    vm.$broadcast('schemaFormValidate');
+    this.$broadcast('schemaFormValidate');
         // Then we check if the form is valid
         if (form.$valid) {
             console.log('Whoot!!! We\'re good to go!!!');
